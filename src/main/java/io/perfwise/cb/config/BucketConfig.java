47f510983887d51870f49e03e0304610623ff666
sthota <sthota@cloudleaf.io>
2023-02-08 11:42:58 +0800

    Disable tls and make a fat jar

--- src/main/java/io/perfwise/cb/config/BucketConfig.java
+++ src/main/java/io/perfwise/cb/config/BucketConfig.java
@@ -1,5 +1,8 @@
 package io.perfwise.cb.config;
 
+import com.couchbase.client.core.deps.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import com.couchbase.client.core.env.SecurityConfig;
+import com.couchbase.client.java.env.ClusterEnvironment;
 import java.io.Serializable;
 import java.time.Duration;
 import java.util.HashMap;
@@ -21,155 +24,159 @@ import com.couchbase.client.core.error.CouchbaseException;
 import static com.couchbase.client.java.ClusterOptions.clusterOptions;
 
 public class BucketConfig extends ConfigTestElement
-		implements ConfigElement, TestBean, TestStateListener, Serializable {
+        implements ConfigElement, TestBean, TestStateListener, Serializable {
 
-	private static final long serialVersionUID = -313826245020004586L;
-	private static final Logger LOGGER = LoggerFactory.getLogger(BucketConfig.class);
+    private static final long serialVersionUID = -313826245020004586L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BucketConfig.class);
 
-	private String username;
-	private String password;
-	private String server;
-	private String bucketName;
-	private String bucketWaitUntilReadyTime;
-	private String connectionObject;
-	private String bucketObject;
-	private Bucket bucket;
-	private Cluster cluster;
-	private List<VariableSettings> extraConfigs;
-	private HashMap<String, Object> objectMap = new HashMap<String, Object>();
+    private String username;
+    private String password;
+    private String server;
+    private String bucketName;
+    private String bucketWaitUntilReadyTime;
+    private String connectionObject;
+    private String bucketObject;
+    private Bucket bucket;
+    private Cluster cluster;
+    private List<VariableSettings> extraConfigs;
+    private HashMap<String, Object> objectMap = new HashMap<String, Object>();
 
-	public BucketConfig() {
-	}
-	@Override
-	public void addConfigElement(ConfigElement config) {
-	}
+    public BucketConfig() {
+    }
 
-	@Override
-	public void testStarted() {
-		this.setRunningVersion(true);
-		TestBeanHelper.prepare(this);
-		JMeterVariables variables = getThreadContext().getVariables();
+    @Override
+    public void addConfigElement(ConfigElement config) {
+    }
 
-		LOGGER.debug("Additional config Size::: " + getExtraConfigs().size());
-		if (getExtraConfigs().size() >= 1) {
-			LOGGER.info("Setting up Additional properties");
-			for (int i = 0; i < getExtraConfigs().size(); i++) {
+    @Override
+    public void testStarted() {
+        this.setRunningVersion(true);
+        TestBeanHelper.prepare(this);
+        JMeterVariables variables = getThreadContext().getVariables();
+
+        LOGGER.debug("Additional config Size::: " + getExtraConfigs().size());
+        if (getExtraConfigs().size() >= 1) {
+            LOGGER.info("Setting up Additional properties");
+            for (int i = 0; i < getExtraConfigs().size(); i++) {
 //				props.put(getExtraConfigs().get(i).getConfigKey(),
 //						getExtraConfigs().get(i).getConfigValue());
-				LOGGER.debug(
-						String.format("Adding property : %s", getExtraConfigs().get(i).getConfigKey()));
-			}
-		}
+                LOGGER.debug(
+                        String.format("Adding property : %s", getExtraConfigs().get(i).getConfigKey()));
+            }
+        }
 
-		if (variables.getObject(bucketObject) != null) {
-			LOGGER.error("Found an active Couchbase connection !!");
-		} else {
-			synchronized (this) {
-				try {
-					cluster = Cluster.connect("couchbases://"+ getServer(), getClusterOptions(getUsername(), getPassword()));
-					objectMap.put("cluster", cluster);
-					LOGGER.info("Connection to couchbase cluster has been established");
-					try {
-						bucket = cluster.bucket(bucketName);
-						bucket.waitUntilReady(Duration.ofSeconds(Integer.parseInt(getBucketWaitUntilReadyTime())));
-						objectMap.put("bucket", bucket);
-					} catch (CouchbaseException e) {
-						LOGGER.info("Exception while creating connection to bucket:" + e);
-					}
-					variables.putObject(bucketObject, objectMap);
-				} catch (Exception e) {
-					LOGGER.info("Failed to create couchbase cluster", e);
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+        if (variables.getObject(bucketObject) != null) {
+            LOGGER.error("Found an active Couchbase connection !!");
+        } else {
+            synchronized (this) {
+                try {
 
-	public ClusterOptions getClusterOptions(String username, String password){
-		return clusterOptions(username, password).environment(env -> {
-			env.applyProfile("wan-development");
-		});
-	}
+                    cluster = Cluster.connect("couchbases://" + getServer(),
+                            getClusterOptions(getUsername(), getPassword()));
+                    objectMap.put("cluster", cluster);
+                    LOGGER.info("Connection to couchbase cluster has been established");
+                    try {
+                        bucket = cluster.bucket(bucketName);
+                        bucket.waitUntilReady(Duration.ofSeconds(Integer.parseInt(getBucketWaitUntilReadyTime())));
+                        objectMap.put("bucket", bucket);
+                    } catch (CouchbaseException e) {
+                        LOGGER.info("Exception while creating connection to bucket:" + e);
+                    }
+                    variables.putObject(bucketObject, objectMap);
+                } catch (Exception e) {
+                    LOGGER.info("Failed to create couchbase cluster", e);
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	@Override
-	public void testStarted(String host) {
-		testStarted();
-	}
+    public ClusterOptions getClusterOptions(String username, String password) {
+        ClusterEnvironment env = ClusterEnvironment.builder()
+                .securityConfig(SecurityConfig.enableTls(false))
+                .build();
+        return clusterOptions(username, password).environment(env);
+    }
 
-	@Override
-	public void testEnded() {
-		try {
-			cluster.disconnect();
-			LOGGER.info("Couchbase cluster connection - Disconnected successfully !");
-		} catch (Exception e) {
-			LOGGER.info("Exception occurred while closing the connection with couchbase server");
-		}
-	}
+    @Override
+    public void testStarted(String host) {
+        testStarted();
+    }
 
-	@Override
-	public void testEnded(String host) {
-		testEnded();
-	}
+    @Override
+    public void testEnded() {
+        try {
+            cluster.disconnect();
+            LOGGER.info("Couchbase cluster connection - Disconnected successfully !");
+        } catch (Exception e) {
+            LOGGER.info("Exception occurred while closing the connection with couchbase server");
+        }
+    }
+
+    @Override
+    public void testEnded(String host) {
+        testEnded();
+    }
 
 // ====== Getters and Setters
 
-	public String getServer() {
-		return server;
-	}
+    public String getServer() {
+        return server;
+    }
 
-	public void setServer(String server) {
-		this.server = server;
-	}
+    public void setServer(String server) {
+        this.server = server;
+    }
 
-	public String getBucketName() {
-		return bucketName;
-	}
+    public String getBucketName() {
+        return bucketName;
+    }
 
-	public void setBucketName(String bucketName) {
-		this.bucketName = bucketName;
-	}
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getBucketWaitUntilReadyTime() {
-		return bucketWaitUntilReadyTime;
-	}
+    public String getBucketWaitUntilReadyTime() {
+        return bucketWaitUntilReadyTime;
+    }
 
-	public void setBucketWaitUntilReadyTime(String bucketWaitUntilReadyTime) {
-		this.bucketWaitUntilReadyTime = bucketWaitUntilReadyTime;
-	}
+    public void setBucketWaitUntilReadyTime(String bucketWaitUntilReadyTime) {
+        this.bucketWaitUntilReadyTime = bucketWaitUntilReadyTime;
+    }
 
-	public List<VariableSettings> getExtraConfigs() {
-		return extraConfigs;
-	}
+    public List<VariableSettings> getExtraConfigs() {
+        return extraConfigs;
+    }
 
-	public void setExtraConfigs(List<VariableSettings> extraConfigs) {
-		this.extraConfigs = extraConfigs;
-	}
+    public void setExtraConfigs(List<VariableSettings> extraConfigs) {
+        this.extraConfigs = extraConfigs;
+    }
 
-	public String getBucketObject() {
-		return bucketObject;
-	}
+    public String getBucketObject() {
+        return bucketObject;
+    }
 
-	public void setBucketObject(String bucketObject) {
-		this.bucketObject = bucketObject;
-	}
+    public void setBucketObject(String bucketObject) {
+        this.bucketObject = bucketObject;
+    }
 
-	public String getConnectionObject(){
-		return connectionObject;
-	}
+    public String getConnectionObject() {
+        return connectionObject;
+    }
 }
